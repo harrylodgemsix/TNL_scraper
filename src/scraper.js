71cf81fa0ca55078e817eeded3d22627c86cc42a
harrylodgemsix <69455955+harrylodgemsix@users.noreply.github.com>
2021-05-12 16:34:25 +0100

    removed and added some variables

--- src/scraper.js
+++ src/scraper.js
@@ -7,8 +7,7 @@ scrape({
     'article_title_last_viewed',
     'article_id_last_viewed',
     'article_image',
-    'page_tags',
-    'customer_id',
+    'page_tags'
   ],
 
   // Optional, enable product retargeting by firing the 'product-viewed' event
@@ -24,24 +23,15 @@ scrape({
 
     url: getUrl,
 
-    // Article Title, e.g. "US election: Trump stokes fresh fury as Biden appeals for calm"
-    article_title: () => window.utag_data.attrs.headline,
-
-    // Article Title Last Viewed, e.g. "us election: trump stokes fresh fury as biden appeals for calm"
-    article_title_last_viewed: () => window.utag_data.article_headline_last_viewed,
-
-    // Article Id Last Viewed, e.g. "6a3ac798-1fb7-11eb-a6f9-33c15bd31337"
-    article_id_last_viewed: () => window.utag_data.article_id_last_viewed,
-
-    // Article Publish Date, e.g. "2020/11/13"
-    article_publish_date: () => window.utag_data.article_publish_date,
-
     // Article Author, e.g. "David Charter"
     article_author: () => window.utag_data.article_author,
 
     // Article Image, e.g. "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F1b819f3e-200f-11eb-8696-f5d5fcef88fd.jpg?crop=4476%2C2518%2C346%2C343&resize=685"
     article_image: () => window.utag_data['meta.og:image'],
-
+    
+    // Article Name, e.g. "inquiry to be held into government’s pandemic response"
+    article_name: () => window.utag_data.article_name,
+    
     // Page Site Name, e.g. "the times and sunday times"
     page_site_name: () => window.utag_data.page_site_name,
 
@@ -60,53 +50,16 @@ scrape({
     // Action, e.g. "Viewed"
     action: () => window.utag_data.action,
 
-    // sp_v1_consent, e.g. "1!1:1:1:0"
-    sp_v1_consent: () => window.utag_data['cp._sp_v1_consent'],
-
-    // Login Event Fired, e.g. "false"
-    login_event_fired: () => window.utag_data['cp.login_event_fired'],
-
-    // NUK Consent Personalisation, e.g. "1"
-    nuk_consent_personalisation: () =>
-      parseFloat(window.utag_data['cp.nuk-consent-personalisation']),
-
-    // NUK Customer Country Code, e.g. "GB"
-    nuk_customer_country_code: () => window.utag_data['cp.nuk_customer_country_code'],
-
-    // NUK Customer Location Hint, e.g. "UK"
-    nuk_customer_location_hint: () => window.utag_data['cp.nuk_customer_location_hint'],
-
-    // NUK Customer Region Code, e.g. "EN"
-    nuk_customer_region_code: () => window.utag_data['cp.nuk_customer_region_code'],
-
-    // NUKT Krux Consent, e.g. "SENT"
-    nukt_krux_consent: () => window.utag_data['cp.nukt_krux_consent'],
-
     // Permutive Id, e.g. "08028893-55b9-4cff-a138-0ce3782035ae"
     permutive_id: () => window.utag_data['cp.permutive-id'],
+    
+    // Page Name, e.g. "article:us election: trump stokes fresh fury as biden appeals for calm"
+    page_name: () => window.utag_data.page_name,
+    
+    // Page Section, e.g. "news"
+    page_section: () => window.utag_data.page_section,
 
-    // Customer Id, e.g. " " - this returns blank for me as I assume you need to be logged in. I Expect it to return a string similar to other Ids
-    customer_id: () => window.utag_data.customer_id,
 
-    // Customer Type, e.g. "guest"
-    customer_type: () => window.utag_data.customer_type,
 
-    // Propensity Threshold, e.g. 0
-    propensity_threshold: () => parseFloat(window.utag_data.propensity_threshold),
-
-    // Propensity Threshold AU, e.g. 0.56
-    propensity_threshold_AU: () => parseFloat(window.utag_data.propensity_threshold_AU),
-
-    // Propensity Threshold CA, e.g. 0.55
-    propensity_threshold_CA: () => parseFloat(window.utag_data.propensity_threshold_CA),
-
-    // Propensity Threshold FR, e.g. 0.63
-    propensity_threshold_FR: () => parseFloat(window.utag_data.propensity_threshold_FR),
-
-    // Propensity Threshold GB, e.g. 0
-    propensity_threshold_GB: () => parseFloat(window.utag_data.propensity_threshold_GB),
-
-    // Propensity Threshold US, e.g. 0.56
-    propensity_threshold_US: () => parseFloat(window.utag_data.propensity_threshold_US),
   },
 });
